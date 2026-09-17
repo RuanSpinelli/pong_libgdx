f6d83a96b0be00b369bc28dbb2938ae2b455110f
Ruan Spinelli <ruanspinelli@gmail.com>
2026-09-16 23:01:14 -0300

    Janela basica que fecha no ESC
    Por hora apenas fiz duas classes, uma para ser a principal do jogo, e a outra para cuidar das responsabilidades da tela. No momento é apenas uma janela preta que ao apertar ESC, o jogo se fecha sozinho

--- core/src/main/java/com/game/Main.java
+++ core/src/main/java/com/game/Main.java
@@ -1,32 +1,58 @@
 package com.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.game.screens.GameScreen;
 
+
+// Classe principal do jogo
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class Main extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+public class Main extends Game {
+	
+	
+	public static Main INSTANCE;
+	private int screenWidth, screenHeight;
+	private OrthographicCamera camera;
+	
+	
+	
+	public Main() {
+		INSTANCE = this;
+	}
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+    	this.screenHeight = Gdx.graphics.getWidth();
+    	this.screenWidth = Gdx.graphics.getHeight();
+    	this.camera = new OrthographicCamera();
+    	this.camera.setToOrtho(false, screenWidth, screenHeight);
+    	
+    	
+    	setScreen(new GameScreen(camera)); //Define uma tela que vai rodar quando o jogo inicializar
     }
 
-    @Override
-    public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
-    }
+	public int getScreenWidth() {
+		return screenWidth;
+	}
 
-    @Override
-    public void dispose() {
-        batch.dispose();
-        image.dispose();
-    }
+	public void setScreenWidth(int screenWidth) {
+		this.screenWidth = screenWidth;
+	}
+
+	public int getScreenHeight() {
+		return screenHeight;
+	}
+
+	public void setScreenHeight(int screenHeight) {
+		this.screenHeight = screenHeight;
+	}
+    
+    
+    
+    
 }
